202b1c922b884592573b4bc3c56dcb00c92f69fc
blackfaded <rene.heinen@gmx.net>
2017-11-06 15:11:49 +0100

    Routen angelegt und dynamisches ausliefern der Seiten

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,54 @@
-var express = require('express');
-var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+// dashboard
+var dashboard = require('./dashboard');
 
-module.exports = router;
+// help
+var help = require('./help/index');
+
+// cronjobs
+var cronjobs = require('./cronjobs/index');
+
+// databases
+var databases = require('./databases/index');
+var postgres = require('./databases/postgres');
+var mongodb = require('./databases/mongodb');
+
+// runtimes
+var runtimes = require('./runtimes/index');
+var nodejs = require('./runtimes/nodejs');
+var php = require('./runtimes/php');
+
+// vcs
+var vcs = require('./vcs/index');
+var gitlab = require('./vcs/gitlab');
+var svn = require('./vcs/svn');
+
+
+
+module.exports = function (app) {
+
+    // dashboard
+    app.use('/', dashboard);
+
+    // help
+    app.use('/help', help);
+
+    // cronjobs
+    app.use('/cronjobs', cronjobs);
+
+    // databases
+    app.use('/databases', databases);
+    app.use('/databases/postgresql', postgres);
+    app.use('/databases/mongodb', mongodb);
+
+    // runtimes
+    app.use('/runtimes', runtimes);
+    app.use('/runtimes/nodejs', nodejs);
+    app.use('/runtimes/php', php);
+
+    // runtimes
+    app.use('/vcs', vcs);
+    app.use('/vcs/nodejs', gitlab);
+    app.use('/vcs/php', svn);
+
+};
