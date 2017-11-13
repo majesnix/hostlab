6ca8f73a45e05c4c875f39777100f7a04daf55f2
blackfaded <rene.heinen@gmx.net>
2017-11-13 10:04:03 +0100

    Shell script test

--- tasks/registerUser.js
+++ tasks/registerUser.js
@@ -1,6 +1,22 @@
 // Beim ersten Login auszuführen um Nutzer anzulegen
 
-module.exports = function (user){
+module.exports = (user) => {
   // Shellscripte ausführen, wie z.B einen Nutzer anlegen, Gitlab anlegen, DB anlegen etc.
+
+    const { spawn } = require('child_process');
+
+
+    //TODO: User anlegen
+    const child = spawn(
+        'ls -l',
+        {
+            stdio: 'inherit',
+            shell: true
+        }
+    );
+
+    child.on('message', (msg) => {
+        console.log(msg);
+    })
 
 };
