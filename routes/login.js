942ffd390a5881732d3eb6609801798da970134c
Blackfaded <rene.heinen@gmx.net>
2017-11-14 12:51:08 +0100

    Login mit Passport implementiert

--- routes/login.js
+++ routes/login.js
@@ -1,27 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
 
 /* GET help page. */
 router.get('/', (req, res, next) => {
     res.render('login', {layout: 'empty', test: "Dummyinhalt"});
 });
 
-router.post('/', (req, res, next) => {
-    res.redirect('/');
-
-    // TODO: Prüfen, ob user existiert, falls nicht registerUser.js requiren und User anlegen
-
- /*   var user = false;
-    if(!user){
-        require('../tasks/registerUser')(user);
-        // TODO: vielleicht mit promises checken, ob anlegen fertig ist und dann aufs dashboard leiten
+router.post('/',passport.authenticate('local-login',
+    {
+        failureRedirect: '/login'
+    }),
+    function (req, res) {
+        // Redirect to originalUrl if set before login
+        res.redirect(req.app.locals.originalUrl || '/');
     }
-    else{
-        // TODO: sonst aufs Dashboard weiterleiten
-        res.redirect('/');
-    }
-*/
-
-});
+);
 
 module.exports = router;
