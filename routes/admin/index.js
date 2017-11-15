896e4bbb8d090d0501f267ccf4a28764972c9217
Blackfaded <rene.heinen@gmx.net>
2017-11-15 14:00:19 +0100

    Strukturelle Anpassungen

--- routes/admin/index.js
+++ routes/admin/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const createUser = require('../../tasks/devhelper/createUser');
+const createUser = require('../../tasks/createUserInDB');
 
 
 /* GET help page. */
