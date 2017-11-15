b7abb9ee0263f82379cb4ddf61a4a03f889c9976
Blackfaded <rene.heinen@gmx.net>
2017-11-15 13:20:04 +0100

    Admin Route zum erstellen von lokalen Nutzern erstellt

--- routes/admin/index.js
+++ routes/admin/index.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const createUser = require('../../tasks/devhelper/createUser');
+
 
 /* GET help page. */
 router.get('/', (req, res, next) => {
-    res.render('admin/index');
+    res.render('admin/index', {user: req.user});
+});
+
+router.post('/newLocalUser', (req, res) => {
+   let username = req.body.username;
+   let password = req.body.password;
+   let admin = req.body.admin;
+
+    createUser(username, 'user123@user123.de', password, admin, true)
+
 });
 
 module.exports = router;
