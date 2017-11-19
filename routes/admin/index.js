ac8a2a0410d465322d551a25a6429e6d2e4c1ae5
jan wystub <jan.wystub@hsrw.org>
2017-11-19 03:20:25 +0100

    Ändere *cant* zu *cannot*

--- routes/admin/index.js
+++ routes/admin/index.js
@@ -3,69 +3,63 @@ const router = express.Router();
 const createUser = require('../../tasks/createUser');
 const deleteUser = require('../../tasks/deleteUser');
 
-
 const User = require('../../databases/mongodb/models/user');
-
 
 /* GET help page. */
 router.get('/', (req, res, next) => {
 
-    User.find({}, function (err, users) {
-        if (err) {
-            console.log(err);
-        }
-        console.log(users);
-        res.render('admin/index', {user: req.user, users: users});
-    });
-
+  User.find({}, function(err, users) {
+    if (err) {
+      console.log(err);
+    }
+    console.log(users);
+    res.render('admin/index', {user: req.user, users: users});
+  });
 
 });
 
 router.post('/user', (req, res) => {
 
-    let opts = {
-        username: req.body.username,
-        password: req.body.password,
-        email: req.body.email,
-        admin: req.body.admin,
-        localuser: true
+  let opts = {
+    username: req.body.username,
+    password: req.body.password,
+    email: req.body.email,
+    admin: req.body.admin,
+    localuser: true,
 
-    };
+  };
 
-    createUser(opts, (err, user) => {
-        if(err){
-            console.log(err);
-        }
-        console.log(user);
-        res.send('ok');
-        //TODO: sonst antworten
-    })
-
+  createUser(opts, (err, user) => {
+    if (err) {
+      console.log(err);
+    }
+    console.log(user);
+    res.send('ok');
+    //TODO: sonst antworten
+  });
 
 });
 
 router.delete('/user/:username', (req, res) => {
 
-    let opts = {
-        username: req.params.username,
-    };
+  let opts = {
+    username: req.params.username,
+  };
 
-    if(req.user.username !== opts.username){
-        deleteUser(opts, (err) => {
-            if(err){
-                console.log(err);
-            }
-            else{
-                res.send('ok');
+  if (req.user.username !== opts.username) {
+    deleteUser(opts, (err) => {
+      if (err) {
+        console.log(err);
+      }
+      else {
+        res.send('ok');
 
-            }
-        })
-    }
-    else {
-        res.send('You cant delete yourself');
-    }
-
-
+      }
+    });
+  }
+  else {
+    res.send('You cannot delete yourself!');
+  }
 
 });
 
