925722e0d6f08771e6a0344bffbf7919b3ed23c5
jan wystub <jan.wystub@hsrw.org>
2017-11-19 02:49:31 +0100

    Laden des Pakets `linux-user` nur auf BS Linux
    Das Paket hielt die App davon ab auf anderen BS als Linux zu starten.
    Das Arbeiten unter Windows wurde dadurch erschwert.
    
    close #38

--- tasks/createUser.js
+++ tasks/createUser.js
@@ -1,40 +1,53 @@
 const User = require('../databases/mongodb/models/user');
-const linuxUser = require('linux-user');
 
+/**
+ * Skip system user operations while testing on other OS than Linux
+ */
+const linuxUser = process.platform === 'linux'
+    ? require('linux-user')
+    : {
+      addUser: () => {
+        console.info('Not running on linux, did not create system user');
+      },
+    };
 
 const createUserinDB = (opts, callback) => {
 
+  let newUser = new User();
+  newUser.username = opts.username;
+  newUser.email = opts.email;
 
-    let newUser = new User();
-    newUser.username = opts.username;
-    newUser.email = opts.email;
-    newUser.generateHash(opts.password, function (err, hash) {
+  newUser.generateHash(opts.password, function(err, hash) {
+    if (err) {
+      return callback(err);
+    }
+    newUser.password = hash;
+  });
+
+  newUser.admin = opts.admin;
+  newUser.localuser = opts.localuser;
+
+  newUser.save(function(err) {
+    if (err) {
+      return callback(err);
+    }
+
+    linuxUser.addUser(opts.username, (err, user) => {
+      if (err) {
+        return callback(err);
+      }
+
+      linuxUser.setPassword(opts.username, opts.password, (err) => {
         if (err) {
-            return callback(err);
-
+          return callback(err);
         }
-        newUser.password = hash;
-    });
-    newUser.admin = opts.admin;
-    newUser.localuser = opts.localuser;
-    newUser.save(function (err) {
-        if (err) {
-            return callback(err);
-        }
-        linuxUser.addUser(opts.username, (err, user) => {
-            if(err){
-                return callback(err);
-            }
-            linuxUser.setPassword(opts.username, opts.password, (err) => {
-                if(err){
-                    return callback(err);
-                }
-                return callback(false, newUser);
-            });
 
-        })
+        return callback(false, newUser);
+      });
 
     });
+
+  });
 
 };
 
--- tasks/deleteUser.js
+++ tasks/deleteUser.js
@@ -1,22 +1,33 @@
 const User = require('../databases/mongodb/models/user');
-const linuxUser = require('linux-user');
 
+/**
+ * Skip system user operations while testing on other OS than Linux
+ */
+const linuxUser = process.platform === 'linux'
+    ? require('linux-user')
+    : {
+      removeUser: function() {
+        console.info('Not running on linux, did not delete system user');
+      },
+    };
 
 const deleteUserinDB = (opts, callback) => {
 
-    User.deleteOne({'username': opts.username}, function (err) {
-        if (err) {
-            console.log(err);
-            return callback(err);
-        }
+  User.deleteOne({'username': opts.username}, function(err) {
+    if (err) {
+      console.log(err);
+      return callback(err);
+    }
 
-        linuxUser.removeUser(opts.username, (err) => {
-            if(err){
-                return callback(err);
-            }
-            return callback(false);
-        })
+    linuxUser.removeUser(opts.username, (err) => {
+      if (err) {
+        return callback(err);
+      }
+
+      return callback(false);
     });
+
+  });
 
 };
 
