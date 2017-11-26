c9416df6352deed5ef55d64f17f3d9e634922b7d
blackfaded <rene.heinen@gmx.net>
2017-11-26 20:13:47 +0100

    User können ihr Passwort ändern

--- routes/settings/index.js
+++ routes/settings/index.js
@@ -1,13 +1,78 @@
 const express = require('express');
 const router = express.Router();
 
+
+const User = require('../../databases/mongodb/models/user');
+
 router.get('/', (req, res, next) => {
-  res.render('settings/index', {user: req.user});
+  res.render('settings/index', {user: req.user, feedback: req.flash('feedback')});
 });
 
-router.put('/changePassword', (req, res, next) => {
-      const pw = req.body.password;
-});
+router.post('/password', (req, res, next) => {
+      const oldPassword = req.body.oldPassword;
+      const newPassword = req.body.newPassword;
+      const newPasswordConfirm = req.body.newPasswordConfirm;
+      console.log(req.user.username);
 
+      if (newPassword === newPasswordConfirm) {
+        User.findOne({'username': req.user.username}, function(err, user) {
+          if (err) {
+            console.error(err);
+          }
+          else if (!user) {
+            req.flash('feedback', 'No user found.');
+            res.redirect('/settings');
+          }
+          else if (!user.localuser) {
+
+          }
+          else {
+            user.validPassword(oldPassword, function(err, response) {
+              if (err) {
+                console.error(err);
+                req.flash('feedback', 'Error on updating your password.');
+                res.redirect('/settings');
+              }
+              else if (response === false) {
+                req.flash('feedback', 'Old password did not match.');
+                res.redirect('/settings');
+              }
+              else {
+                user.generateHash(newPassword, (err, hash) => {
+                  if (err) {
+                    console.error(err);
+                    req.flash('feedback', 'Error on updating your password.');
+                    res.redirect('/settings');
+                  }
+                  else {
+                    user.password = hash;
+
+                    user.save((err) => {
+                      if (err) {
+                        req.flash('feedback', 'Error on updating your password.');
+                        res.redirect('/settings');
+                      }
+                      else {
+                        req.flash('feedback', 'Password changed successfully.');
+                        res.redirect('/settings');
+                      }
+                    });
+                  }
+
+                });
+
+              }
+            });
+          }
+
+        });
+      }
+      else {
+        req.flash('feedback', 'New passwords did not match.');
+        res.redirect('/settings');
+      }
+
+    },
+);
 
 module.exports = router;
