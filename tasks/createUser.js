b50c2c84e4ba1299b18dafeddd5507ef4ef88342
parideis <parideis@users.noreply.github.com>
2017-11-27 19:17:46 +0100

    Add gitlab user creation

--- tasks/createUser.js
+++ tasks/createUser.js
@@ -1,10 +1,29 @@
 const User = require('../databases/mongodb/models/user');
+// request Modul ermöglich das posten an den gitlab Server
+const request = require('request');
 
 const createUser = (opts, callback) => {
 
   let newUser = new User();
   newUser.username = opts.username;
   newUser.email = opts.email;
+
+  // gitlab optionen die zur Nutzergenerierung benötigt werden
+  let gitlabopts = {
+      email: opts.email,
+      username: opts.username,
+      name: opts.username,
+      password: opts.password,
+      admin: opts.admin
+  };
+  // gitlab post request zur Erstellunge des Gitlab Nutzer
+  // TODO: get Token from ENV variable
+  request.post({url:'http://gitlab.local/api/v4/users?private_token=ztMRuozmaWCL7BEtjNwv', formData: gitlabopts}, function (err, httpResponse, body) {
+      if (err) {
+          return console.error('Git User creation failed', err);
+      }
+      console.log('Git User Created:', body);
+  });
 
   newUser.generateHash(opts.password, function(err, hash) {
     if (err) {
