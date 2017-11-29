df25e42848aa164bd92c9f3f2e7e094f31a8ef08
student <parideis@users.noreply.github.com>
2017-11-29 18:05:06 +0100

    Add Save Repo Archive (static project)

--- routes/api/container.js
+++ routes/api/container.js
@@ -1,27 +1,42 @@
 const router = require('express').Router();
 const util = require('util');
 const {exec} = require('child_process');
+const request = require('request');
+const fs = require('fs');
 
 router.post('/', (req, res, next) => {
+  console.log("Erfolgreich in der Post");
   let http_url_to_repo = req.body.http_url_to_repo;
   let type = req.body.type;
-
-  exec('mktemp -d', (error, stdout, stderr) => {
+  exec('mktemp -d', (error, tmp_folder, stderr) => {
     if (error) {
       console.error(`exec error: ${error}`);
       return;
     }
-    console.log(`stdout: ${stdout}`);
+    tmp_folder = tmp_folder.trim();
+    console.log(`stdout: ${tmp_folder}`);
     console.log(`stderr: ${stderr}`);
-    exec(util.format('git clone %s %s', http_url_to_repo, stdout),
-        (error, stdout, stderr) => {
-          if (error) {
-            console.error(`exec error: ${error}`);
-            return;
-          }
+    /*
+    request.get({url:'http://gitlab.local/api/v4/projects/2/repository/archive?private_token='+process.env.GITLAB_TOKEN}, function (err, httpResponse, body) {
+      if (err) {
+        return console.error('Git get User Repositries failed', err);
+      }
+      let buff = new Buffer(body);
+      fs.writeFile('/home/student/archive', buff,'binary', (err) =>{
+        if (err) throw err;
+        console.log('File has been saved');
+      });
+    }); 
+    */
 
-        });
-
+    exec(util.format('wget http://gitlab.local/api/v4/projects/2/repository/archive?private_token=%s\
+    -O '+tmp_folder+'/archive.tar.gz', process.env.GITLAB_TOKEN), function(error, stdout, stderr){
+      if (error) {
+        console.error(`exec error: ${error}`);
+        return;
+      }
+      console.log(`Repo Saved`);
+    });
   });
 });
 
