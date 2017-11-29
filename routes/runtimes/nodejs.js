0aee25a4639c5a192ca71714b7d0a3863b931e43
student <parideis@users.noreply.github.com>
2017-11-29 14:34:25 +0100

    Show Repositories in Add Server Modal

--- routes/runtimes/nodejs.js
+++ routes/runtimes/nodejs.js
@@ -1,7 +1,20 @@
 const router = require('express').Router();
+const request = require('request');
 
 router.get('/', (req, res, next) => {
-  res.render('runtimes/nodejs');
+  const gitlab_id = req.user.gitlab_id;
+  request.get({url:'http://gitlab.local/api/v4/users/'+gitlab_id+'/projects?private_token='+process.env.GITLAB_TOKEN}, function (err, httpResponse, body) {
+    if (err) {
+      return console.error('Git get User Repositries failed', err);
+    }
+    console.log('Got Git User Repositories');
+    body = JSON.parse(body);
+    let repositories = [];
+    body.forEach((project) => {
+      repositories.push(project.name);
+    });
+    res.render('runtimes/nodejs', {repositories:repositories});
+  }); 
 });
 
 module.exports = router;
