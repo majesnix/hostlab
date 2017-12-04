715f709100fc416a864b98abf44430ad3ac478ea
jan wystub <jan.wystub@hsrw.org>
2017-12-04 03:16:18 +0100

    Paket dockerode hinzugefügt und Docker-API angesprochen

--- config/connections.js
+++ config/connections.js
@@ -2,4 +2,10 @@ module.exports = {
   mongo: {
     url: process.env.MONGO_URL || 'mongodb://hostlab.local/hostlab',
   },
+  docker: {
+    socketPath: process.env.DOCKER_SOCKET || '/var/run/docker.sock',
+    host: process.env.DOCKER_HOST,
+    port: process.env.DOCKER_PORT || 2375,
+    protocol: process.env.DOCKER_PROTOCOL || 'http',
+  },
 };
--- routes/api/container.js
+++ routes/api/container.js
@@ -1,21 +1,25 @@
 const router = require('express').Router();
+const log = require('debug')('hostlab:route:api');
 const util = require('util');
 const {exec} = require('child_process');
 const request = require('request');
-const fs = require('fs');
+const fs = require('fs');
+const path = require('path');
+const {docker, dockerfile} = require('../../config/docker');
 
 router.post('/', (req, res, next) => {
-  console.log("Erfolgreich in der Post");
-  let http_url_to_repo = req.body.http_url_to_repo;
-  let type = req.body.type;
+  log('Erfolgreich in der Post');
+  log(req.body);
+  const {http_url_to_repo, type} = req.body;
+
   exec('mktemp -d', (error, tmp_folder, stderr) => {
     if (error) {
-      console.error(`exec error: ${error}`);
-      return;
+      log(`exec error: ${error}`);
+      return next(error);
     }
-    tmp_folder = tmp_folder.trim();
-    console.log(`stdout: ${tmp_folder}`);
-    console.log(`stderr: ${stderr}`);
+    tmp_folder = tmp_folder.trim();
+    log(`stdout: ${tmp_folder}`);
+    log(`stderr: ${stderr}`);
     /*
     request.get({url:'http://gitlab.local/api/v4/projects/2/repository/archive?private_token='+process.env.GITLAB_TOKEN}, function (err, httpResponse, body) {
       if (err) {
@@ -24,19 +28,44 @@ router.post('/', (req, res, next) => {
       let buff = new Buffer(body);
       fs.writeFile('/home/student/archive', buff,'binary', (err) =>{
         if (err) throw err;
-        console.log('File has been saved');
+        log('File has been saved');
       });
-    }); 
+    });
     */
 
-    exec(util.format('wget http://gitlab.local/api/v4/projects/2/repository/archive?private_token=%s\
-    -O '+tmp_folder+'/archive.tar.gz', process.env.GITLAB_TOKEN), function(error, stdout, stderr){
-      if (error) {
-        console.error(`exec error: ${error}`);
-        return;
-      }
-      console.log(`Repo Saved`);
-    });
+    if (type.includes('node')) {
+      const archive = 'archive.tar.gz';
+      exec(util.format(
+          'wget http://gitlab.local/api/v4/projects/2/repository/archive?' +
+          'private_token=%s -O %s',
+          process.env.GITLAB_TOKEN, path.join(tmp_folder, archive)),
+          function(error, stdout, stderr) {
+            if (error) {
+              console.error(`exec error: ${error}`);
+              return next(error);
+            }
+            log(`Repo Saved`);
+            fs.writeFile(path.join(tmp_folder, 'Dockerfile'),
+                dockerfile.node(archive), 'utf-8', function(err) {
+                  if (err) {
+                    log('writeFile:', err);
+                    return next(err);
+                  }// Kein Fehler beim Schreiben
+                  docker.buildImage({
+                    context: tmp_folder,
+                    src: ['Dockerfile', archive],
+                  }, {t: 'nodeimage'}, function(err, output) {
+                    if (err) {
+                      log('buildImage', err);
+                      return next(err);
+                    }// Kein Fehler beim Image erstellen
+                    output.pipe(process.stdout);
+                    // Erfolgreich erstellt --> 201 Created
+                    res.status(201).end();
+                  });
+                });
+          });
+    }// else
   });
 });
 
