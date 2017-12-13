c7b47c1a28183bd31d16d4f8047928d4dca5ab8a
jan wystub <jan.wystub@hsrw.org>
2017-12-13 01:00:42 +0100

    Include some hostlab-b routes + fancy middleware

--- routes/index.js
+++ routes/index.js
@@ -10,17 +10,19 @@ const node = require('./node');
 const postgres = require('./postgres');
 const settings = require('./settings');
 
+const adminsettings = require('./adminsettings');
+const databases = require('./databases');
+const usersettings = require('./usersettings');
+
 module.exports = (app) => {
   app.use('/login', login);
 
-  /**
-   * Ab hier können die Routen nur noch als registrierter Benutzer aufgerufen werden
-   */
-  app.use(isRegistered);
+  app.use(ensureAuthentication);
   app.use('/logout', logout);
   app.use('/api', api);
 
-  app.use(exposeReqInfos);
+  app.use(activateNavElems);
+  app.use(passFlashMessages);
   app.use('/', dashboard);
   app.use('/help', help);
   app.use('/mongo', mongoExpress(mongoExpressConfig));
@@ -28,50 +30,46 @@ module.exports = (app) => {
   app.use('/postgres', postgres);
   app.use('/settings', settings);
 
+  app.use('/databases', databases);
+  app.use('/usersettings', usersettings);
+
   /**
    * Ab hier können die Routen nur noch als Administrator aufgerufen werden
    */
-  app.use(isAdmin);
+  app.use(ensureBeingAdmin);
   app.use('/admin', admin);
+  app.use('/adminsettings', adminsettings);
 };
 
-/**
- * Helferfunktion um registrierte Nutzer zu identifizieren
- */
-function isRegistered(req, res, next) {
+function ensureAuthentication(req, res, next) {
   if (req.isAuthenticated()) {
     // Reiche das User-Objekt an die nächsten Routen weiter
     res.locals.user = req.user;
-
-    // Weitermachen
     return next();
   }
   res.redirect('/login');
 }
 
-/**
- * Helferfunktion um Administrator zu identifizieren
- */
-function isAdmin(req, res, next) {
+function ensureBeingAdmin(req, res, next) {
   if (req.user.isAdmin) {
     // Zeige Adminansicht statt Nutzeransicht
     res.locals.layout = 'admin';
-
-    // Weitermachen
     return next();
   }
   res.redirect('/');
 }
 
-/**
- * Helferfunktion für Vorverarbeitungen des Requests
- */
-function exposeReqInfos(req, res, next) {
+function activateNavElems(req, res, next) {
   // Damit die Rendering-Engine weiß auf welchem Navigationselement wir uns
   // befinden, stellen wir ihr eine entsprechende Variable zur Verfügung.
   const splitURL = req.path.split('/');
   const navPath = splitURL[splitURL.length - 1] || 'dashboard';
   const activeNav = 'nav-' + navPath;
   res.locals[activeNav] = true;
+  next();
+}
+
+function passFlashMessages(req, res, next) {
+  res.locals['message'] = req.flash();
   next();
 }
