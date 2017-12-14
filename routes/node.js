51273a44d5cd0de13df5bf2931e6a14b37b2d76b
MajesNix <dominic@dominic-classen.de>
2017-12-14 01:22:34 +0100

    fixes #57, refactor request to snekfetch, display error message when no gitlab_id is found or snekfetch return error

--- routes/node.js
+++ routes/node.js
@@ -1,35 +1,42 @@
 const router = require('express').Router();
 const log = require('debug')('hostlab:route:node');
-const request = require('request');
-const util = require('util');
+//const request = require('request');
+//const util = require('util');
+const snek = require('snekfetch');
 const gitlab_token = process.env.GITLAB_TOKEN ||
     require('../config/gitlab_token').gitlab_token;
+const gitlab_url = process.env.GITLAB_URL;
 
-router.get('/', (req, res, next) => {
-  const gitlab_id = req.user.gitlab_id;
-  request.get({
-    url: util.format(
-        'http://gitlab.local/api/v4/users/%d/projects?private_token=%s',
-        gitlab_id, gitlab_token),
-  }, function(err, httpResponse, body) {
-    if (err) {
-      log('Request failed', err);
-      return next(err);
+router.get('/', async (req, res, next) => {
+  try {
+    const gitlab_id = req.user.gitlab_id;
+
+    if (gitlab_id) {
+      const { text } = await snek.get(`https://${gitlab_url}/api/v4/users/${gitlab_id}/projects?private_token=${gitlab_token}`);
+
+      const projects = JSON.parse(text);
+      
+      const repositories = [];
+      for (let project of projects) {
+        repositories.push({
+          path: project.path_with_namespace,
+          repo_url: project.http_url_to_repo,
+        });
+      }
+
+      res.render('node', { repositories });
+    } else {
+      res.locals.message.error = '[HOSTLAB] Gitlab ID not found';
+      res.render('node');
     }
-    log(httpResponse);
-    log('Request finished');
-    log(body);
-    projects = JSON.parse(body);
-    log(projects);
-    const repositories = [];
-    for (let project of projects) {
-      repositories.push({
-        path: project.path_with_namespace,
-        repo_url: project.http_url_to_repo,
-      });
+  } catch (err) {
+    if (err.text) {
+      res.locals.message.error = `[GITLAB] ${err.text}`;
+      res.render('node');
     }
-    res.render('node', {repositories});
-  });
+    console.error(err);
+    //return next(err);
+  }
 });
 
 module.exports = router;
