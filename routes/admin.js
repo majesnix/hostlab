1bac8ac8679b94d63f9f13a95f740df5dda553e4
blackfaded <rene.heinen@gmx.net>
2017-12-16 19:38:02 +0100

    updated GET admin/users/:id updated GET admin/users/:id/edit

--- routes/admin.js
+++ routes/admin.js
@@ -20,7 +20,6 @@ router.get('/', (req, res, next) => {
 router.get('/users', (req, res, next) => {
   let users;
   // Hole alle Nutzer
-  log(req.headers)
   request.get(`http://localhost:${req.app.settings.port}/api/v1/users`).
       set('cookie', req.headers.cookie).
       then((r) => {
@@ -40,34 +39,29 @@ router.get('/users/create', (req, res, next) => {
 router.get('/users/:id', (req, res, next) => {
   const {id} = req.params;
   // Hole bestimmten Nutzer
-  User.findById(id, function(err, user) {
-    if (err) {
-      return next(err);
-    }
-    log(user);
-    if (!user) {
-      return next();
-    }
-    // Zeige Nutzerdaten
-    res.render('usersShow', {user});
-
-  });
+  request.get(`http://localhost:${req.app.settings.port}/api/v1/users/${id}`).
+      set('cookie', req.headers.cookie).
+      then((r) => {
+        log(r.body);
+        const user = r.body;
+        // Zeige alle Nutzer
+        res.render('usersShow', {user});
+      });
 });
 
 router.get('/users/:id/edit', (req, res, next) => {
   const {id} = req.params;
   // Hole bestimmten Nutzer
-  User.findById(id, function(err, user) {
-    if (err) {
-      return next(err);
-    }
-    log(user);
-    if (!user) {
-      return next();
-    }
-    // Zeige Editierseite für bestimmten Nutzer
-    res.render('usersEdit', {user});
-  });
+  request.get(`http://localhost:${req.app.settings.port}/api/v1/users/${id}`).
+      set('cookie', req.headers.cookie).
+      then((r) => {
+        log(r.body);
+        const user = r.body;
+        // Zeige Editierseite für bestimmten Nutzer
+        res.render('usersEdit', {user});
+      });
+
+
 });
 
 module.exports = router;
