b230d06557736f60232e8c32593ce1ec50368212
Rene <rene.heinen@gmx.net>
2017-12-20 19:09:02 +0100

    Reject promise without gitlab account

--- tasks/createUser.js
+++ tasks/createUser.js
@@ -4,11 +4,12 @@ const snek = require('snekfetch'); // Handles all http requests
 
 const gitlab_token = process.env.GITLAB_TOKEN ||
     require('../config/gitlab').gitlab_token;
-const gitlab_url = process.env.GITLAB_URL || require('../config/gitlab').gitlab_url;
+const gitlab_url = process.env.GITLAB_URL ||
+    require('../config/gitlab').gitlab_url;
 
 module.exports = async (opts) => {
   // Create new Promise
-  return new Promise( async (resolve,reject) => {
+  return new Promise(async (resolve, reject) => {
 
     try {
       // Erstelle neuen Nutzer aus Schema
@@ -23,11 +24,12 @@ module.exports = async (opts) => {
       // GitlabURL wird aus der Env-Variable "GITLAB_URL" gelesen
 
       // gets ALL gitlab users
-      const {text} = await snek.get(`${gitlab_url}/api/v4/users?private_token=${gitlab_token}`);
+      const {text} = await snek.get(
+          `${gitlab_url}/api/v4/users?private_token=${gitlab_token}`);
 
       // parse Gitlab response to json
       const users = JSON.parse(text);
-      
+
       // filter users by email (should return the wanted user, because emails should be unique)
       const foundUser = users.filter(u => u.email === opts.email);
 
@@ -40,15 +42,13 @@ module.exports = async (opts) => {
       // save gitlab_id to database
       if (foundUser.length === 1) {
         newUser.gitlab_id = foundUser[0].id;
-        // when the user is created through the admin interface, the gitlab avatar_url is null, this show a default hostlab avatar
-        if (foundUser[0].avatar_url) {
-          newUser.avatar_url = foundUser[0].avatar_url;
-        } else {
-          newUser.avatar_url = '/vendor/assets/default.png';
-        }
+        newUser.avatar_url = foundUser[0].avatar_url
+            ? foundUser[0].avatar_url
+            : '/vendor/assets/default.png';
       } else {
-        newUser.gitlab_id = null;
-        newUser.avatar_url = '/vendor/assets/default.png';
+        return reject(
+            new Error('You need an active Gitlab Account on ' + gitlab_url +
+                ' to use this service.'));
       }
 
       const dbUser = await newUser.save();
@@ -58,7 +58,8 @@ module.exports = async (opts) => {
     } catch (err) {
       log(err);
       if (err.message.includes('getaddrinfo ENOTFOUND')) {
-        return reject(new Error('Gitlab is temporarily not available, please try again later'));
+        return reject(new Error(
+            'Gitlab is temporarily not available, please try again later'));
       } else {
         return reject(err);
       }
