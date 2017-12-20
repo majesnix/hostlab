28c101d4b86030d2e0e917bd03ceb833f36be44a
Rene <rene.heinen@gmx.net>
2017-12-20 19:01:42 +0100

    Copy Gitlab Config on first creation

--- bin/scripts/gitlab-docker.sh
+++ bin/scripts/gitlab-docker.sh
@@ -2,15 +2,18 @@
 #
 # run gitlab in docker
 
+export GITLAB_CONFIG=/media/sf_hostlab/config/gitlab.rb
+
 sudo docker run --name gitlab \
 --detach --restart always \
 --hostname gitlab.local \
---publish 127.0.0.11:443:443 \
---publish 127.0.0.11:80:80 \
---publish 127.0.0.11:22:22 \
+--publish 192.168.178.53:443:443 \
+--publish 192.168.178.53:80:80 \
+--publish 192.168.178.53:9022:22 \
 --volume /srv/gitlab/config:/etc/gitlab \
 --volume /srv/gitlab/logs:/var/log/gitlab \
 --volume /srv/gitlab/data:/var/opt/gitlab \
 gitlab/gitlab-ce:latest
 
-sudo sh -c 'echo "\n127.0.0.11\tgitlab.local" >> /etc/hosts'
+sudo sh -c 'echo "\n192.168.178.53\tgitlab.local" >> /etc/hosts'
+sudo docker cp ${GITLAB_CONFIG} gitlab:/etc/gitlab/gitlab.rb
