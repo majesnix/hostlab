ad5ad08cc8c5d59ebd49fda4a783bc4772eca6c3
Rene <rene.heinen@gmx.net>
2018-01-03 18:50:58 +0100

    WIP: Starte Nutzer Docker Container

--- config/docker.js
+++ config/docker.js
@@ -15,7 +15,8 @@ ADD ${file} .
 RUN mv */* .
 RUN npm install
 ENV PORT=8080
-EXPOSE 8080`,
+EXPOSE 8080
+CMD ["npm", "start"]`,
 };
 
 module.exports = {docker, dockerfile};
--- routes/api/container.js
+++ routes/api/container.js
@@ -1,72 +1,83 @@
 const router = require('express').Router();
-const util = require('util');
-const {exec} = require('child_process');
-//const request = require('request');
+const tmp = require('tmp');
+const snek = require('snekfetch');
 const fs = require('fs');
 const path = require('path');
 const {docker, dockerfile} = require('../../config/docker');
 const log = require('debug')('hostlab:route:api:container');
+const gitlab_token = process.env.GITLAB_TOKEN;
+const gitlab_url = process.env.GITLAB_URL;
 
-router.post('/', (req, res, next) => {
-  log('Erfolgreich in der Post');
-  log(req.body);
-  const {http_url_to_repo, type} = req.body;
-
-  exec('mktemp -d', (error, tmp_folder, stderr) => {
-    if (error) {
-      log(`exec error: ${error}`);
-      return next(error);
+router.post('/:repositoryID', (req, res, next) => {
+  const {repositoryID} = req.params;
+  log('Creating Container with Repository ID:', repositoryID);
+  const archive = 'archive.tar.gz';
+  tmp.dir({
+    template: '/tmp/tmp-XXXXXX',
+    unsafeCleanup: true,
+  }, function(err, tempPath, removeCallback) {
+    if (err) {
+      throw err;
     }
-    tmp_folder = tmp_folder.trim();
-    log(`stdout: ${tmp_folder}`);
-    log(`stderr: ${stderr}`);
-    /*
-    request.get({url:'http://gitlab.local/api/v4/projects/2/repository/archive?private_token='+process.env.GITLAB_TOKEN}, function (err, httpResponse, body) {
-      if (err) {
-        return console.error('Git get User Repositries failed', err);
-      }
-      let buff = new Buffer(body);
-      fs.writeFile('/home/student/archive', buff,'binary', (err) =>{
-        if (err) throw err;
-        log('File has been saved');
-      });
-    });
-    */
-
-    if (type.includes('node')) {
-      const archive = 'archive.tar.gz';
-      exec(util.format(
-          'wget http://gitlab.local/api/v4/projects/2/repository/archive?' +
-          'private_token=%s -O %s',
-          process.env.GITLAB_TOKEN, path.join(tmp_folder, archive)),
-          function(error, stdout, stderr) {
-            if (error) {
-              console.error(`exec error: ${error}`);
-              return next(error);
-            }
-            log(`Repo Saved`);
-            fs.writeFile(path.join(tmp_folder, 'Dockerfile'),
-                dockerfile.node(archive), 'utf-8', function(err) {
-                  if (err) {
-                    log('writeFile:', err);
-                    return next(err);
-                  }// Kein Fehler beim Schreiben
-                  docker.buildImage({
-                    context: tmp_folder,
-                    src: ['Dockerfile', archive],
-                  }, {t: 'nodeimage'}, function(err, output) {
-                    if (err) {
-                      log('buildImage', err);
-                      return next(err);
-                    }// Kein Fehler beim Image erstellen
-                    output.pipe(process.stdout);
-                    // Erfolgreich erstellt --> 201 Created
-                    res.status(201).end();
-                  });
-                });
-          });
-    }// else
+    console.log('Dir: ', tempPath);
+    snek.get(
+        `${gitlab_url}/api/v4/projects/${repositoryID}/repository/archive?private_token=${gitlab_token}`).
+        then((response) => {
+          fs.writeFile(path.join(tempPath, archive), response.body,
+              (err) => {
+                if (err) {
+                  log(err);
+                }
+                log('File Saved.');
+                fs.writeFile(path.join(tempPath, 'Dockerfile'),
+                    dockerfile.node(archive), 'utf-8', function(err) {
+                      if (err) {
+                        log('writeFile:', err);
+                        return next(err);
+                      }// Kein Fehler beim Schreiben
+                      docker.buildImage({
+                        context: tempPath,
+                        src: ['Dockerfile', archive],
+                      }, {t: 'nodeimage'}, function(err, output) {
+                        if (err) {
+                          log('buildImage', err);
+                          return next(err);
+                        }// Kein Fehler beim Image erstellen
+                        output.pipe(process.stdout, {
+                          end: true,
+                        });
+                        output.on('end', function() {
+                          docker.createContainer({
+                            Image: 'nodeimage',
+                            ExposedPorts: {
+                              '8080/tcp': {},
+                            },
+                            Hostconfig: {
+                              Privileged: true,
+                              PortBindings: {
+                                '8080/tcp': [
+                                  {
+                                    HostPort: '5000',
+                                  }],
+                              },
+                            },
+                          }, function(err, container) {
+                            container.start(function(err, data) {
+                              if (err) return;
+                            });
+                          });
+                        });
+                      });
+                    });
+              });
+        });
   });
+});
+
+router.delete('/:repositoryID', (req, res, next) => {
+  const {repositoryID} = req.params;
+  log('Deleting Container with Repository ID:', repositoryID);
+  res.send(repositoryID);
 });
 
 module.exports = router;
