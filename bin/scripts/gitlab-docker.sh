e96486b6298b3160ac7250dcf26cb482142c748a
jan wystub <jan.wystub@hsrw.org>
2018-01-12 14:59:35 +0100

    Replace repeating ip with new env within the script

--- bin/scripts/gitlab-docker.sh
+++ bin/scripts/gitlab-docker.sh
@@ -2,18 +2,19 @@
 #
 # run gitlab in docker
 
-export GITLAB_CONFIG=/media/sf_gits/hostlab/config/gitlab.rb
+export EXTERNAL_IP=192.168.56.102
+export GITLAB_CONFIG=/media/sf_hostlab/config/gitlab.rb
 
 sudo docker run --name gitlab \
 --detach --restart always \
 --hostname gitlab.local \
---publish 10.71.34.101:10443:443 \
---publish 10.71.34.101:10080:80 \
---publish 10.71.34.101:10022:22 \
+--publish ${EXTERNAL_IP}:443:443 \
+--publish ${EXTERNAL_IP}:80:80 \
+--publish ${EXTERNAL_IP}:22:22 \
 --volume /srv/gitlab/config:/etc/gitlab \
 --volume /srv/gitlab/logs:/var/log/gitlab \
 --volume /srv/gitlab/data:/var/opt/gitlab \
 gitlab/gitlab-ce:latest
 
-sudo sh -c 'echo "\n10.71.34.101\tgitlab.local" >> /etc/hosts'
+sudo sh -c 'echo "\n${EXTERNAL_IP}\tgitlab.local" >> /etc/hosts'
 sudo docker cp ${GITLAB_CONFIG} gitlab:/etc/gitlab/gitlab.rb
