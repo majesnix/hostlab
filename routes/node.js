f3cf908ec48de064258b371d5eb0b0c120c2f04b
parideis <parideis@users.noreply.github.com>
2018-01-14 15:28:38 +0100

    [WIP] User Container Console Log

--- routes/node.js
+++ routes/node.js
@@ -1,11 +1,50 @@
 const router = require('express').Router();
 const log = require('debug')('hostlab:route:node');
 const snek = require('snekfetch');
+const {docker, dockerfile} = require('../config/docker');
+const stream = require('stream');
 const gitlab_token = process.env.GITLAB_TOKEN;
 const gitlab_url = process.env.GITLAB_URL;
 
+// WIP: Container Log
+let cl = "";
 router.get('/', async (req, res, next) => {
+  // Example get MongoDB log
+  let container = docker.getContainer('378d0392deb8');
+  console.log(container);
+ /**
+ * Get logs from running container
+ */
+function containerLogs(container) {
+
+  // create a single stream for stdin and stdout
+  const logStream = new stream.PassThrough();
+  logStream.on('data', function(chunk){
+    cl += chunk.toString('utf8');
+    console.log(chunk.toString('utf8'));
+  });
+
+  container.logs({
+    follow: true,
+    stdout: true,
+    stderr: true
+  }, function(err, stream){
+    if(err) {
+      return logger.error(err.message);
+    }
+    container.modem.demuxStream(stream, logStream, logStream);
+    stream.on('end', function(){
+      logStream.end('!stop!');
+    });
+
+    setTimeout(function() {
+      stream.destroy();
+    }, 1000);
+  });
+}
+
   try {
+    containerLogs(container);
     const gitlab_id = req.user.gitlab_id;
 
     if (gitlab_id) {
@@ -19,6 +58,7 @@ router.get('/', async (req, res, next) => {
           repositories.push({
             id: project.id,
             name: project.name,
+            logs: cl,
             path: project.path_with_namespace,
             repo_url: project.http_url_to_repo,
           });
