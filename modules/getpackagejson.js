ecae84980f9a29467feb987e3713e0b253357fe7
MajesNix <dominic@dominic-classen.de>
2018-01-21 14:48:26 +0100

    added license info unified language in docs

--- modules/getpackagejson.js
+++ modules/getpackagejson.js
@@ -1,19 +1,38 @@
+/*
+ * This file is part of HostLab.
+ *
+ * Copyright 2017 Dominic Claßen
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 const snek = require('snekfetch');
 const gitlab_token = process.env.GITLAB_TOKEN;
 const gitlab_url = process.env.GITLAB_URL;
 
 /**
- * Holt die PackageJSON vom übergebenen Gitlab Repository (ID) und dem angegebenen Branch
+ * Retrieves the PackageJSON of the given Gitlab Repository (ID) and the given Branch
  * @param {string} repositoryID The GitlabID of the repository
  * @param {string} [branch=master] The branch of the repository
  * @returns {Promise<Object>}
  * @example
- * // Holt die Package.json vom Projekt mit der ID 1 und dem Branch 'master'
+ * // Retrieves the Package.json of the project with the ID 1 and branch 'master'
  * const info = await getPackageJSON('1');
  * 
- * // Holt die Package.json vom Projekt mit der ID 1 und dem Branch 'test'
+ * // Retrieves the Package.json of the project with the ID 1 and branch 'test'
  * const info = await getPackageJSON('1','test');
  */
+
 module.exports = (repositoryID, branch = 'master') => {
 
     return new Promise(async (resolve,reject) => {
