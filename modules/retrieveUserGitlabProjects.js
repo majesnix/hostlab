5fbe4f2275b7642aac4ebdc4f5bb2053538a3125
majesnix <dominic@dominic-classen.de>
2018-01-22 13:10:41 +0100

    renaming envs (should work now)

--- modules/retrieveUserGitlabProjects.js
+++ modules/retrieveUserGitlabProjects.js
@@ -3,31 +3,31 @@ const snek = require('snekfetch');
 const log = require('debug')('hostlab:module:retrieveUserGitlabProjects');
 
 module.exports = async function(userGitlabId) {
-    const { text } = await snek.get(`${process.env.GITLAB_URL}/api/v4/projects?private_token=${process.env.GITLAB_TOKEN}&sudo=${userGitlabId}&membership=true`);
-    const projects = JSON.parse(text);
+	const { text } = await snek.get(`${process.env.GITLAB_URL}/api/v4/projects?private_token=${process.env.GITLAB_TOKEN}&sudo=${userGitlabId}&membership=true`);
+	const projects = JSON.parse(text);
 
-    const repositories = [];
+	const repositories = [];
 
-    for(let project of projects) {
-        let packagejson;
-        try {
-            packagejson = (await snek.head(`${process.env.GITLAB_URL}/api/v4/projects/${project.id}/repository/files/package.json?private_token=${process.env.GITLAB_TOKEN}&ref=master`)) ? true : false;
-        } catch (err) {
-            log(err)
-        }
+	for (let project of projects) {
+		let packagejson;
+		try {
+			packagejson = (await snek.head(`${process.env.GITLAB_URL}/api/v4/projects/${project.id}/repository/files/package.json?private_token=${process.env.GITLAB_TOKEN}&ref=master`)) ? true : false;
+		} catch (err) {
+			// Catches snekfetch promise rejection, no need to process this.
+		}
 
-        if (!project.archived && packagejson) {
-            let projBranches = JSON.parse((await snek.get(project._links.repo_branches + `?private_token=${process.env.GITLAB_TOKEN}`)).text);
+		if (!project.archived && packagejson) {
+			let projBranches = JSON.parse((await snek.get(project._links.repo_branches + `?private_token=${process.env.GITLAB_TOKEN}`)).text);
 
-            repositories.push({
-                id: project.id,
-                name: project.name,
-                path: project.path_with_namespace,
-                repo_url: project.http_url_to_repo,
-                branches: projBranches.map(e=>e.name)
-            });
-        }
-    }
+			repositories.push({
+				id: project.id,
+				name: project.name,
+				path: project.path_with_namespace,
+				repo_url: project.http_url_to_repo,
+				branches: projBranches.map(e=>e.name)
+			});
+		}
+	}
 
-    return repositories;
+	return repositories;
 };
--- routes/api/blueprint.js
+++ routes/api/blueprint.js
@@ -6,38 +6,38 @@ const gitlab_token = process.env.GITLAB_TOKEN;
 const gitlab_url = process.env.GITLAB_URL;
 
 router.post('/', async (req, res, next) => {
-    const blueprintName = req.param("name");
-    const blueprintRepoID = req.param("repo")[0];
-    const blueprintRepoBranch = req.param("repo")[1]
+	const blueprintName = req.param('name');
+	const blueprintRepoID = req.param('repo')[0];
+	const blueprintRepoBranch = req.param('repo')[1];
 
-    const response = await snek.get(`${gitlab_url}/api/v4/projects/${blueprintRepoID}?private_token=${gitlab_token}`);
-    const blueprintRepoName = JSON.parse(response.text).name
+	const response = await snek.get(`${gitlab_url}/api/v4/projects/${blueprintRepoID}?private_token=${gitlab_token}`);
+	const blueprintRepoName = JSON.parse(response.text).name;
 
-    User.findByIdAndUpdate(req.user._id, {
-        $push: {
-            'blueprints.node': {
-                name: `${blueprintName}`,
-                containingRepoName: `${blueprintRepoName}`,
-                containingRepoID: `${blueprintRepoID}`,
-                containingRepoBranch: `${blueprintRepoBranch}`,
-            },
-        },
-    }, (err, user) => {
-        if (err) {
-            return next(err);
-        }
-        res.send(200);
-    });
+	User.findByIdAndUpdate(req.user._id, {
+		$push: {
+			'blueprints.node': {
+				name: `${blueprintName}`,
+				containingRepoName: `${blueprintRepoName}`,
+				containingRepoID: `${blueprintRepoID}`,
+				containingRepoBranch: `${blueprintRepoBranch}`,
+			},
+		},
+	}, (err, user) => {
+		if (err) {
+			return next(err);
+		}
+		res.send(200);
+	});
 });
 
 router.delete('/:id', async (req, res, next) => {
-    const blueprintID = req.param("id");
+	const blueprintID = req.param('id');
 
-    User.findById(req.user._id, function(err, user) {
-        user.blueprints.node.id(blueprintID).remove();
-        user.save();
-        res.send(200);
-    })
+	User.findById(req.user._id, function(err, user) {
+		user.blueprints.node.id(blueprintID).remove();
+		user.save();
+		res.send(200);
+	});
 
 });
 
