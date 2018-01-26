48b8bbac105f611f6f7b72475360e6c854d77264
jan wystub <jan.wystub@hsrw.org>
2018-01-27 00:50:11 +0100

    Replaced js and css files with minified versions

--- gulpfile.js
+++ gulpfile.js
@@ -23,20 +23,20 @@ const concat = require('gulp-concat');
 
 gulp.task('css', function() {
   return gulp.src([
-    './node_modules/bootstrap/dist/css/bootstrap.css',
-    './node_modules/bootstrap/dist/css/bootstrap-grid.css',
-    './node_modules/bootstrap/dist/css/bootstrap-reboot.css',
-    './node_modules/admin-lte/dist/css/AdminLTE.css',
-    './node_modules/admin-lte/dist/css/skins/skin-black.css',
+    './node_modules/bootstrap/dist/css/bootstrap.min.css',
+    './node_modules/bootstrap/dist/css/bootstrap-grid.min.css',
+    './node_modules/bootstrap/dist/css/bootstrap-reboot.min.css',
+    './node_modules/admin-lte/dist/css/AdminLTE.min.css',
+    './node_modules/admin-lte/dist/css/skins/skin-black.min.css',
   ]).pipe(concat('bundle.css')).pipe(gulp.dest('./public/css/'));
 });
 
 gulp.task('js', function() {
   return gulp.src([
-    './node_modules/jquery/dist/jquery.js',
-    './node_modules/bootstrap/dist/js/bootstrap.js',
-    './node_modules/icheck/icheck.js',
-    './node_modules/admin-lte/dist/js/adminlte.js',
+    './node_modules/jquery/dist/jquery.min.js',
+    './node_modules/bootstrap/dist/js/bootstrap.min.js',
+    './node_modules/icheck/icheck.min.js',
+    './node_modules/admin-lte/dist/js/adminlte.min.js',
   ]).pipe(concat('bundle.js')).pipe(gulp.dest('./public/js/'));
 });
 
