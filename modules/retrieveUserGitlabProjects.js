70776422b752a0916448bedfcbaa946a34f3fb5b
jawys <jan.wystub@hsrw.org>
2018-01-28 23:59:53 +0100

    Update retrieveUserGitlabProjects.js
    Adds username before repository name

--- modules/retrieveUserGitlabProjects.js
+++ modules/retrieveUserGitlabProjects.js
@@ -39,21 +39,18 @@ module.exports = async function(userGitlabId) {
     } catch (err) {
       // Catches snekfetch promise rejection (thrown when the project has no package.json), no need to process this.
     }
-
     if (!project.archived && packagejson) {
       let projBranches = JSON.parse(
           (await snek.get(project._links.repo_branches +
               `?private_token=${process.env.GITLAB_TOKEN}`)).text);
-
       repositories.push({
         id: project.id,
-        name: project.name,
+        name: project.name_with_namespace,
         path: project.path_with_namespace,
         repo_url: project.http_url_to_repo,
         branches: projBranches.map(e => e.name),
       });
     }
   }
-
   return repositories;
 };
